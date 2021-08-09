6aba6bc301ba416169ca3c310b1f8c1373e6939d
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-08-09 10:16:52 -0400

    +bouldering image

--- src/Intro.js
+++ src/Intro.js
@@ -13,6 +13,7 @@ class Intro extends React.Component{
         <p>안녕, 나는 컴퓨터 공학 학생 이에요.</p>
         <br></br>
         <p>This is who I am.</p>
+        <img src={require("./images/bouldering.jpeg").default}/>
       </div>
     )
   }
