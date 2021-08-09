35353122b116b7664e81b35a9aa4cb0db015713c
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-08-09 17:05:53 -0400

    +projects page

---
+++ src/Projects.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Projects extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return(
+
+    )
+  }
+}
+
+export default Projects;
