00cb744d57f7dd3b09093dae2181f9723b962b4f
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-08-09 14:51:00 -0400

    introduction grid css

--- src/Intro.js
+++ src/Intro.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Intro.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class Intro extends React.Component{
   constructor(props){
@@ -8,10 +9,20 @@ class Intro extends React.Component{
   render(){
     return(
       <div>
-        <p>Xin chào, tôi là một sinh viên khoa học máy tính.</p>
-        <p>Bonjour, je suis un étudiant d&#39;informatique.</p>
-        <p>Hello, I am a Computer Science student.</p>
-        <p>안녕, 나는 컴퓨터 공학 학생 이에요.</p>
+				<div id="introduction" class="d-flex flex-column align-items-start">
+					<div>
+						<p>Xin chào, tôi là một sinh viên khoa học máy tính.</p>
+					</div>
+					<div>
+						<p>Bonjour, je suis un étudiant d&#39;informatique.</p>
+					</div>
+					<div>
+						<p>Hello, I am a Computer Science student.</p>
+					</div>
+					<div>
+						<p>안녕, 나는 컴퓨터 공학 학생 이에요.</p>
+					</div>
+				</div>
         <br></br>
         <p>This is who I am.</p>
         <img id="code" src={require("./images/kid_computer.jpeg").default}/>
