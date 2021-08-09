b9c1821f30295c8572745a0ebc3af5bcf2b0d552
Linh Hoang <trong.hoang@mail.mcgill.ca>
2021-08-09 17:01:46 -0400

    fixed identation

--- src/Intro.js
+++ src/Intro.js
@@ -9,33 +9,33 @@ class Intro extends React.Component{
   render(){
     return(
       <div>
-				<div id="introduction" class="d-flex flex-column align-items-start">
-					<div>
-						<p>Xin chào, tôi là một sinh viên khoa học máy tính.</p>
-					</div>
-					<div>
-						<p>Bonjour, je suis un étudiant d&#39;informatique.</p>
-					</div>
-					<div>
-						<p>Hello, I am a Computer Science student.</p>
-					</div>
-					<div>
-						<p>안녕하세요, 저는 컴퓨터 공학 학생 이에요.</p>
-					</div>
-				</div>
+        <div id="introduction" class="d-flex flex-column align-items-start">
+          <div>
+            <p>Xin chào, tôi là một sinh viên khoa học máy tính.</p>
+          </div>
+          <div>
+            <p>Bonjour, je suis un étudiant d&#39;informatique.</p>
+          </div>
+          <div>
+            <p>Hello, I am a Computer Science student.</p>
+          </div>
+          <div>
+            <p>안녕하세요, 저는 컴퓨터 공학 학생 이에요.</p>
+          </div>
+        </div>
         <br></br>
         <p>This is who I am currently.</p>
-				<div class="d-flex justify-content-around">
-						<div class="p">
-							<img id="code" src={require("./images/kid_computer.jpeg").default}/>
-						</div>
-						<div class="p">
-							<img id="bouldering" src={require("./images/bouldering.jpeg").default} style={{height: '450px'}}/>
-						</div>
-						<div class="p">
-							<img id="dancing" src={require("./images/dancing4.jpg").default}/>
-						</div>
-				</div>
+        <div class="d-flex justify-content-around">
+          <div class="p">
+            <img id="code" src={require("./images/kid_computer.jpeg").default}/>
+          </div>
+          <div class="p">
+            <img id="bouldering" src={require("./images/bouldering.jpeg").default} style={{height: '450px'}}/>
+          </div>
+          <div class="p">
+            <img id="dancing" src={require("./images/dancing4.jpg").default}/>
+          </div>
+        </div>
         <br></br>
         <br></br>
         <a href="https://github.com/Linhguinii" target="__blank">
